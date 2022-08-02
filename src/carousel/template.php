628309e6fdfd0777e0054559c6d73641afca7aa5
techking5287 <techking5287@gmail.com>
2022-08-02 16:02:05 +0100

    Basic carousel template

--- src/carousel/template.php
+++ src/carousel/template.php
@@ -13,16 +13,10 @@
  */
 
 ?>
-<p <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<?php
-	if ( isset( $attributes['message'] ) ) {
-		/**
-		 * The wp_kses_post function is used to ensure any HTML that is not allowed in a post will be escaped.
-		 *
-		 * @see https://developer.wordpress.org/reference/functions/wp_kses_post/
-		 * @see https://developer.wordpress.org/themes/theme-security/data-sanitization-escaping/#escaping-securing-output
-		 */
-		echo wp_kses_post( $attributes['message'] );
-	}
-	?>
-</p>
+<section <?php echo wp_kses_data( get_block_wrapper_attributes( [ 'class' => 'splide' ] ) ); ?>>
+	<div class="splide__track">
+		<div class="splide__list">
+			<?php echo wp_kses_post( $content ); ?>
+		</div>
+	</div>
+</section>
