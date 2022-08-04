cee22a0bb080b63cc1b081d339bae548f36fdb04
techking5287 <techking5287@gmail.com>
2022-08-04 15:23:38 +0100

    Add icon for carousel

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5db201e7d776b7279057');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6eaeb3b9e9039012ddd3');
--- src/carousel/template.php
+++ src/carousel/template.php
@@ -16,7 +16,7 @@
 <section <?php echo wp_kses_data( get_block_wrapper_attributes( [ 'class' => 'splide' ] ) ); ?>>
 	<div class="splide__track">
 		<div class="splide__list">
-			<?php echo wp_kses_post( $content ); ?>
+			<?php echo $content; ?>
 		</div>
 	</div>
 </section>
